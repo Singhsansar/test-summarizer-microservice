eb3976a5c617843e9af77e46652913001591ffa2
Nikhil Singh <92269431+Singhsansar@users.noreply.github.com>
2023-12-25 22:16:11 +0530

    Update model_predection.py
    changed the format

--- model_predection.py
+++ model_predection.py
@@ -2,14 +2,26 @@ import torch
 from transformers import PegasusTokenizer, PegasusForConditionalGeneration
 import os
 
-model_path= "model"
+model_path = "model"
 model = PegasusForConditionalGeneration.from_pretrained(model_path)
 tokenizer = PegasusTokenizer.from_pretrained(model_path)
 
-def predict(text:str,length:int=100):
+
+def predict(text: str, length: int = 20):
     test_summaries = []
-    single_input_encoding = tokenizer(text, return_tensors='pt', max_length=1000, truncation=True, padding=True)
+    single_input_encoding = tokenizer(
+        text, return_tensors="pt", max_length=1000, truncation=True, padding=True
+    )
     with torch.no_grad():
-        generated_ids_single = model.generate(single_input_encoding['input_ids'], max_length=length, num_beams=4, length_penalty=2.0, early_stopping=True)
-        generated_summary_single = tokenizer.decode(generated_ids_single[0], skip_special_tokens=True)
+        generated_ids_single = model.generate(
+            single_input_encoding["input_ids"],
+            max_length=length,
+            min_length=5,
+            num_beams=4,
+            length_penalty=2.0,
+            early_stopping=True,
+        )
+        generated_summary_single = tokenizer.decode(
+            generated_ids_single[0], skip_special_tokens=True
+        )
     return generated_summary_single
