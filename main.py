9e531cbfa9410d3998a89c26e9ef65654377df03
Nikhil Singh <92269431+Singhsansar@users.noreply.github.com>
2023-12-25 22:15:40 +0530

    Update main.py
    added filter

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
-import uvicorn 
-from fastapi import FastAPI
-from pydantic import BaseModel
+import json
+import re
+import uvicorn
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 from model_predection import predict
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
 class Data_aspect(BaseModel):
     text: str
-    length: int = 500
-
+    length: int = 20
 
 @app.post("/predict")
 async def summary(data: Data_aspect):
-    print(f"predict_story accepted json payload: {data}")
-    summary = predict(data.text, data.length)
-    print(f"The result is the following payload: {summary}")
-    payload = {"Summary": summary.tolist()[0]}
-    json_compatible_item_data = jsonable_encoder(payload)
-    return JSONResponse(content=json_compatible_item_data)
+    """Predict the summary of a story"""
+    try:
+        clean_text = re.sub(r'\s+', ' ', data.text)
+        clean_text = re.sub(r'[^\x00-\x7F]+', '', clean_text)
+        summary_text = predict(clean_text, data.length).encode('unicode_escape').decode('utf-8')
+        
+        result = {"Summary": summary_text}
+        json_result = JSONResponse(content=json.dumps(result, ensure_ascii=False))
+        return json_result
+    except Exception as e:
+        logging.error(f"Error processing request: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello You are connected to the API of the project"}
+    return {"message": "Hello! You are connected to the API of the project"}
+
+@app.get("/add/{num1}/{num2}")
+async def add(num1: int, num2: int):
+    """Add two numbers together"""
+    return {"result": num1 + num2}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
